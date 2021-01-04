65adfefe7b9d74592a2f69ad15134cab759a25df
goldmand <49034106+goldmand@users.noreply.github.com>
2021-01-04 15:43:41 +0200

    before part b

--- search_engine.py
+++ search_engine.py
@@ -52,7 +52,7 @@ def run_engine(corpus_path=None, output_path="posting", stemming=False):
                         tweet_list = r.read_file(new_path)  # holds list of the tweets as text
                         idx = parse_and_index_tweet_list(tweet_list, p, indexer, idx)
                         total_num_of_docs += idx
-
+#
     #d2 = datetime.strptime(datetime.now().strftime(fmt), fmt)
     #d2_ts = time.mktime(d2.timetuple())
     #print("[" + str(datetime.now()) + "] " + "Finished Parsing and Indexing documents in " + str(
